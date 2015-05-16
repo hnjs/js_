e5d21d3456c4783b571a2956ca02d440819baad1
harish <harishn@gmx.com>
2015-05-16 12:35:15 +0530

    Rewrote code using more higher order functions and arrays

--- wordify.js
+++ wordify.js
@@ -2,64 +2,44 @@
 // Date 		: 15 May 2015
 // Description 	: Code to convert number to words
 
-function Wordify (num) {
-	if (isNaN(num)) {
-		throw "Invalid input. Numbers only";
-	}
-	var t = num.toString().split(".");
-	this.intValue = t[0]||0;
-	this.decValue = t[1]||0;
-}
+var ones_teens 	= ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 
+					'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+var tens 		= ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+var scales 		= ['', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 
+					'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion'];
 
-// convert the number into an array, where each element is a group of 'size' digits 
-Wordify.prototype.digitGrouping = function(size) {
-	var group 	= [],
-		n 		= this.intValue,
-		divisor = Math.pow(10, size);
+function toWords(n) {	
+	var line;
+	if (isNaN(n) || n > 999) { return null; }
 
-	while (n > 0) {
-		group[group.length] = n % divisor;
-		n = Math.floor(n / divisor);
-	}
-	return group.reverse();
-}
-
-function writeSentence(myArray){
-	var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-	var teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-	var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-	var scales = [
-						'', 'thousand', 
-						'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 
-						'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion'
-					];
-	
-	function digitToWords(n) {
-		var line = "";
-		if (isNaN(n) || n > 999) { return null; }
-
-		if (n == 0) { line = ""; }
-		else if (n < 10) { line = ones[n]; }
-		else if (n < 20) { line = teens[n % 10]; }
-		else if (n < 100) { line = tens[Math.floor(n / 10)] + " " + ones[n % 10]; }
-		else { line = ones[Math.floor(n / 100)] + " hundred " + digitToWords(n % 100); }
-
-		return line.trim();
-	}
-
-	myArray.reverse();
-
-	return myArray.reduce(function(accumulator, item, index, arr){
-		var word = digitToWords(item);
-		if (word) {
-			accumulator = word + " " + scales[index] + " " + accumulator;	
-			//if (index == 0 && word.indexOf("and")<0) { accumulator = "and " + accumulator; }
+	if (n == 0) { line = ""; }
+	else if (n < 20) { line = ones_teens[n]; }
+	else if (n < 100) {
+		line = tens[(n / 10) | 0]; 
+		if (n%10) {
+			line += "-" + ones_teens[n % 10];
 		}
-		return accumulator;
-	},"").trim();
+	} else { line = ones_teens[(n / 100) | 0] + " hundred " + toWords(n % 100); }
+
+	return line.trim();
 }
 
+function chunkify(n) {
+	var numbers = [];
+	while (n > 0) {
+		numbers[numbers.length] = n % 1000;
+		n = Math.floor(n / 1000);
+	}
+	return numbers;
+}
 
-var number = new Wordify(998720047200400);
-console.log("The input number is " + number.intValue);
-console.log(writeSentence(number.digitGrouping(3)));
+function scalify(item, index) {
+	if (item) {return item + " " + scales[index];}
+	return "";
+}
+
+function notEmpty(item) {
+	return !!item; // using double ! returns boolean value. !!item is not same as item.
+}
+
+console.log(chunkify(999999999999999).map(toWords).map(scalify).filter(notEmpty).reverse().join(", "));
